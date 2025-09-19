723503c3a363e6b343c4becccee0baed3604cb43
yasminignacio <ignacio.yasmin@escola.pr.gov.br>
2025-09-19 12:11:43 -0300

    Add files via upload

---
+++ exer9.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 10;
+
+// Operadores aritméticos
+console.log("Soma:", a + b);
+console.log("Subtração:", a - b);
+console.log("Multiplicação:", a * b);
+console.log("Divisão:", a / b);
+console.log("Resto:", a % b);
+
+// Operadores lógicos e relacionais
+console.log("a é maior que b?", a > b);
+console.log("a é menor que b?", a < b);
+console.log("a é igual a b?", a === b);
+
+// Operador lógico AND e OR
+let condicao = (a > 0 && b > 0) || (a < 0);
+console.log("Condição lógica", condicao);
